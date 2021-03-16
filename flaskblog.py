36d02ea4e5f7c4e4c67dc585a25e074201147717
unknown <sharma.anurag1995@gmail.com>
2021-03-16 13:34:51 +0530

    added form and their validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bcf398418d2915b84ef2299e1d37002d'
+# To generate this key, following code is used:
+# import secrets
+# secrets.token_hex(16)
+
 posts_data = [
     {
      'author': 'Anurag Sharma',
@@ -29,6 +37,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Successfully logged in!!', 'success')
+        return redirect(url_for('home')) #redirecting to home page through home method
+    return render_template('register.html', title='Register', form = form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
